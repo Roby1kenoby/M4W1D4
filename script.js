df1cb2b77792b411f672902ebf8fbb1cdca73ca5
Roby1kenoby <r0by1kenoby@hotmail.it>
2024-05-25 19:06:39 +0200

    Finished

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const trackUrl = 'https://striveschool-api.herokuapp.com/api/deezer/album/'
 let cardHtml = ''
 
 let authorSection = document.getElementById('author')
-
+let tracksModal = document.getElementById('tracksModal')
 
 // funzione per recuperare l'html di una card già fatta, così è più semplice creare le altre. 
 // Poi la cancello
@@ -27,7 +27,7 @@ let search = function(){
     startSearchWithAuthor(author)
 }
 
-// funzione per trovare gli album tramite fetch, a partire da un url
+// funzione per trovare gli album di una utore tramite fetch, a partire da un url
 let findAlbums = async function(url, authorId){
     const resp =  await fetch(url)
     let data = await resp.json()
@@ -55,7 +55,7 @@ let cycleAlbums = function(albumArray){
     }
 }
 
-// funzione per creare la singola card di un album e la relativa sezione
+// funzione per creare la singola card di un album
 let createAlbumCard = function(album){
     let cardImage = cardHtml.querySelectorAll('img')[0]
     cardImage.setAttribute('src', album.cover)
@@ -66,7 +66,7 @@ let createAlbumCard = function(album){
     let albumAuthor = cardHtml.querySelectorAll('p')[0]
     albumAuthor.innerHTML = album.artist
 
-    cardHtml.setAttribute('onClick', `getAlbum("${album.tracklist}")`)
+    cardHtml.setAttribute('onClick', `getAlbum("${album.tracklist}", "${album.titolo}")`)
     
     cardHtml.classList.remove('d-none')
     // console.log(cardHtml)
@@ -75,7 +75,7 @@ let createAlbumCard = function(album){
     // console.log(authorSection)
 }
 
-// funzione che dato l'autore, cerca gli album e li inserisce nel dom
+// funzione che dato l'autore, cerca gli album e li inserisce nel dom (funzione iniziale)
 let startSearchWithAuthor = async function(author){
     let completeUrl = albumUrl + author
     let [albumData, authorId] =  await  findAlbums(completeUrl, author)
@@ -84,22 +84,75 @@ let startSearchWithAuthor = async function(author){
 }
 
 // funzione per far apparire il modale con le tracce
-let getAlbum = async function(trackList){
+let getAlbum = async function(trackList, titolo){
     let songsArray = await findTracks(trackUrl+trackList)
-    console.log(songsArray)
+    // aggiorno il titolo
+    tracksModal.querySelector('h3').innerHTML = titolo
+
+    // pulisco il modale
+    cleanTracks()
+
+    // creo le righe per il modale
+    songsArray.forEach((s,i) => {
+        createTracksRow(s,i+1)
+    })
+
+    // apro il modale
     $("#tracksModal").modal()
+}
+
+// funzione per pulire le tracce dal modale
+let cleanTracks = function(){
+    let tab = tracksModal.querySelector('tbody')
+    
+    // individuo la prima row
+    let childDaEliminare = tab.querySelectorAll('tr')[0]
+    
+    // finché ci sono row da eliminare le rimuovo
+    while (childDaEliminare){
+        tab.removeChild(childDaEliminare)
+        childDaEliminare = tab.querySelectorAll('tr')[0]
+    }
+}
+
+// funcione per creare una riga della tabella nel modale
+let createTracksRow = function(song, i){
+    let tab = tracksModal.querySelector('tbody')
+    // creo una riga
+    let row = document.createElement('tr')
+    // creo il primo td con il numero della canzone
+    let nr = document.createElement('td')
+    nr.innerHTML = i
+    row.appendChild(nr)
+    
+    // ciclo per ogni chiave dell'array e creo un elemento td per la row
+    Object.keys(song).forEach(key => {
+        let r = document.createElement('td')
+        r.innerHTML = song[key]
+        row.appendChild(r)
+    });
+
+    // creo il td per il pulsante Play
+    let play = document.createElement('td')
+    play.innerHTML = 'Play'
+    row.appendChild(play)
+
+    // appendo il tutto alla tabella
+    tab.appendChild(row)
+
 }
 
 // funzione per individuare le tracce di un album
 let findTracks = async function(url){
     const resp =  await fetch(url)
     let data = await resp.json()
+    // console.log(data)
     let songs = []
     data.tracks.data.map((s) => {
         let song = {
             title: s.title_short,
-            duration: new Date(s.duration * 1000).toISOString().substring(14, 19),
-            artist: s.artist.name
+            artist: s.artist.name,
+            duration: new Date(s.duration * 1000).toISOString().substring(14, 19)
         }
         songs.push(song)
     });
@@ -109,6 +162,6 @@ let findTracks = async function(url){
 
 
 // fast load album
-onload = (e) => {
-    startSearchWithAuthor('static-x')
-}
+// onload = (e) => {
+//     startSearchWithAuthor('static-x')
+// }
